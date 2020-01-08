2fb1595bd73ea6e5ebc026bcdbdf8614b199e19d
Ramzy Badawy <Ramzy.Badawy@gmail.com>
2020-01-08 16:17:09 -0500

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,45 @@
 class Person
-
 end 
 
 adele_goldberg = Person.new 
-alan_kay = Person.new 
+alan_kay = Person.new
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Person
+
+# end 
+
+# adele_goldberg = Person.new 
+# alan_kay = Person.new 
